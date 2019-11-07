0a4c644f21f4a5524deda237fc067ed4612f6c29
Tiago <tiago.martins.santos@polarising.com>
2019-11-07 18:03:48 +0000

    Rewards registry melhorado

---
+++ SpringLigacaoTibco/src/main/java/com/academyproject/championsacademyleague/services/dateFormatter.java
@@ -0,0 +1,15 @@
+package com.academyproject.championsacademyleague.services;
+
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class dateFormatter {
+
+    public String DateFormatter(){
+        SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd");
+        return String.valueOf(formatter.format(new Date()));
+    }
+}
